da20bbf4bb1674571b33a07668f70f27db388626
fatemehnorouzi <fatemeh.norouzi69@gmail.com>
2025-10-15 09:18:04 -0400

    added deep.py

---
+++ Excercises/deep.py
@@ -0,0 +1,7 @@
+while True:
+      answer= input("What is the answer to the Great Question of Life, the Universe and Everything? ")
+      if (answer == '42') or (answer =='forty-two') or (answer =='forty two'):
+        print("Yes")
+      else:
+          print("No")
+          break
